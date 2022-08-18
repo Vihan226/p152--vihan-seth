660a3d01c5b96c94c648a94673c9eba8add8e0aa
Vihan226 <71296969+Vihan226@users.noreply.github.com>
2022-08-18 16:54:04 -0400

    Add files via upload

---
+++ car.js
@@ -0,0 +1,40 @@
+AFRAME.registerComponent('base', {
+
+    schema:{
+
+        radius :{type:'number', default: 150},
+        height :{type:'number', default: 3},
+    },
+
+    init: function() {
+        this.el.setAttribute('geometry', {
+            primitive: 'cylinder', 
+            radius: this.data.radius,
+            height: this.data.height
+
+        });
+
+        this.el.setAttribute('material', {color: "#1769aa"})
+    },
+
+    update: function(){
+
+        window.addEventListener('click', e =>{
+
+            this.data.clickCounter = this.data.clickCounter + 1;
+
+                if(this.data.clickCounter === 1) {
+                    const rotation = { x:0, y:20, z:0 };
+                    this.el.setAttribute('rotation', rotation);
+                }  
+                else if (this.data.clickCounter === 2){
+                    const rotation = { x:0, y:100, z:0};
+                }
+
+
+        })
+    }
+
+
+
+})
